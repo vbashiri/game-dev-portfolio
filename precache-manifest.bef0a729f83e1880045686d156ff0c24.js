e09a5f1109685084f96b3609fd0e3b71eb68ab32
vbashiri <vbashiri1995@gmail.com>
2023-09-07 18:42:31 +0330

    Updates

---
+++ precache-manifest.bef0a729f83e1880045686d156ff0c24.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad1752338b8f78b02bc64b6914e9d08",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b06cde05708e150d2aed",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "66c39edca8966ef82069",
+    "url": "/portfolio/static/css/main.42c16485.chunk.css"
+  },
+  {
+    "revision": "b06cde05708e150d2aed",
+    "url": "/portfolio/static/js/2.af3e8a73.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.af3e8a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c39edca8966ef82069",
+    "url": "/portfolio/static/js/main.4a728352.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  },
+  {
+    "revision": "8566d0492c1c2a974acdabf2ffc6a257",
+    "url": "/portfolio/static/media/Core.8566d049.png"
+  },
+  {
+    "revision": "42293a6625787d511d7de6364b50c581",
+    "url": "/portfolio/static/media/Ged.42293a66.jpg"
+  },
+  {
+    "revision": "780878819146b8e505d054ab3764bd3e",
+    "url": "/portfolio/static/media/Nightmare.78087881.jpg"
+  },
+  {
+    "revision": "d9bb5518a096a493fac7fca73b6276a3",
+    "url": "/portfolio/static/media/Niloo.d9bb5518.jpg"
+  },
+  {
+    "revision": "103209daf87dd066c194ad2a1abd2b38",
+    "url": "/portfolio/static/media/ParandThe.103209da.jpg"
+  },
+  {
+    "revision": "d7e4a8f488ccde22a01e0a01322ff93f",
+    "url": "/portfolio/static/media/Rebel1.d7e4a8f4.png"
+  },
+  {
+    "revision": "5dc37ea3fb3bcf9cd64073cc218f0743",
+    "url": "/portfolio/static/media/Rebel2.5dc37ea3.png"
+  },
+  {
+    "revision": "7a887b14fed9035c56fd54f3d7e1d2c2",
+    "url": "/portfolio/static/media/Rebel3.7a887b14.png"
+  },
+  {
+    "revision": "1e1b7ef55fd449412c957bf9eab49b81",
+    "url": "/portfolio/static/media/Rebel4.1e1b7ef5.png"
+  },
+  {
+    "revision": "8ef290bd4000723658d5bd6e06560e7e",
+    "url": "/portfolio/static/media/Strygwyr.8ef290bd.JPG"
+  },
+  {
+    "revision": "0a4edea5be28c8baf3d4429c1f662ed3",
+    "url": "/portfolio/static/media/StunningLook.0a4edea5.jpg"
+  },
+  {
+    "revision": "1891c80ecfd36ff5acae660b90d6918f",
+    "url": "/portfolio/static/media/Vahid.1891c80e.webp"
+  },
+  {
+    "revision": "22e446b2db060fa1c4f9be19514390af",
+    "url": "/portfolio/static/media/core1.22e446b2.png"
+  },
+  {
+    "revision": "abb4b0f04f9570aa36c6f932c85f2569",
+    "url": "/portfolio/static/media/core2.abb4b0f0.png"
+  },
+  {
+    "revision": "9b953508dc56a723e30591394a1d7c38",
+    "url": "/portfolio/static/media/core3.9b953508.png"
+  },
+  {
+    "revision": "9dbd475a8f095d3d567ffbf0f95b5e14",
+    "url": "/portfolio/static/media/f_cload1.9dbd475a.png"
+  },
+  {
+    "revision": "3186e57f6b57c29330a7d6ce97328966",
+    "url": "/portfolio/static/media/flyinghills1.3186e57f.png"
+  },
+  {
+    "revision": "a0a96c58658f5e786c04ba55abe5f949",
+    "url": "/portfolio/static/media/flyinghills2.a0a96c58.png"
+  },
+  {
+    "revision": "dce17e7e653b76b5a17110ae37906435",
+    "url": "/portfolio/static/media/flyinghills3.dce17e7e.png"
+  },
+  {
+    "revision": "fec1f12173038a3b4438a9212b995c5c",
+    "url": "/portfolio/static/media/flyinghills4.fec1f121.jpg"
+  },
+  {
+    "revision": "136d47ac34aae5b9a78bab891be696a1",
+    "url": "/portfolio/static/media/flyinghills5.136d47ac.jpg"
+  },
+  {
+    "revision": "b1a48b1b7d8078ff2bc005816a57c0f1",
+    "url": "/portfolio/static/media/gameplay.b1a48b1b.jpg"
+  },
+  {
+    "revision": "03b7ccdbbacb5377c1f91ff4f0404bf4",
+    "url": "/portfolio/static/media/gameplay1.03b7ccdb.png"
+  },
+  {
+    "revision": "97fcc0aabdbe17daf4bf09542aaf0ea6",
+    "url": "/portfolio/static/media/gameplay2.97fcc0aa.jpg"
+  },
+  {
+    "revision": "fafeaaa812230409b97e3ffcc9b39f2d",
+    "url": "/portfolio/static/media/gameplay2.fafeaaa8.png"
+  },
+  {
+    "revision": "2d2dcb0a76151107c8a0c59404181c0d",
+    "url": "/portfolio/static/media/gameplay3.2d2dcb0a.png"
+  },
+  {
+    "revision": "3cc8b99675115cf542111b25971d4158",
+    "url": "/portfolio/static/media/icon.3cc8b996.png"
+  },
+  {
+    "revision": "b2ad6b23c9e476230285ac6c58a22777",
+    "url": "/portfolio/static/media/icon.b2ad6b23.jpg"
+  },
+  {
+    "revision": "2a5b656719b0e7af17f781a941e8e71f",
+    "url": "/portfolio/static/media/iust.2a5b6567.png"
+  },
+  {
+    "revision": "7d567d492cfcf795ca1c115031f75e9b",
+    "url": "/portfolio/static/media/jump_up.7d567d49.png"
+  },
+  {
+    "revision": "f6a95955c8b21560b3c08b4198634aac",
+    "url": "/portfolio/static/media/jumpup1.f6a95955.jpg"
+  },
+  {
+    "revision": "78827e1b2c1cd9de4d465a19d8cb0e61",
+    "url": "/portfolio/static/media/jumpup2.78827e1b.jpg"
+  },
+  {
+    "revision": "fd92bf0fa36add87b75481abd1dac044",
+    "url": "/portfolio/static/media/jumpup3.fd92bf0f.jpg"
+  },
+  {
+    "revision": "eec3bc083838afcf5c35d62f38c7bf49",
+    "url": "/portfolio/static/media/lake.eec3bc08.png"
+  },
+  {
+    "revision": "b857847293a228f1f889f334fb6f72a6",
+    "url": "/portfolio/static/media/mushroom.b8578472.png"
+  },
+  {
+    "revision": "01cba651866ddba76b5d703b84b3aac3",
+    "url": "/portfolio/static/media/ninjamagic0.01cba651.jpg"
+  },
+  {
+    "revision": "a19a5f33aec67b11001991d9cd843f83",
+    "url": "/portfolio/static/media/ninjamagic1.a19a5f33.jpg"
+  },
+  {
+    "revision": "e503f7d1afc685855ee9a9fc5b43e19d",
+    "url": "/portfolio/static/media/ninjamagic2.e503f7d1.jpg"
+  },
+  {
+    "revision": "73b34f99458da78ae10687d6626b1314",
+    "url": "/portfolio/static/media/ninjamagic3.73b34f99.jpg"
+  },
+  {
+    "revision": "89f4f936bbc39e556cf6d12f669dbc71",
+    "url": "/portfolio/static/media/ninjamagic4.89f4f936.jpg"
+  },
+  {
+    "revision": "1eddd48d708561f5c453ef6c2c77af01",
+    "url": "/portfolio/static/media/rebel_forground.1eddd48d.gif"
+  },
+  {
+    "revision": "07119f7daa1dcca728ec26c89402b0e0",
+    "url": "/portfolio/static/media/sky.07119f7d.gif"
+  },
+  {
+    "revision": "a9070dd5ec1fb7357f5add96a53e3aa3",
+    "url": "/portfolio/static/media/spidercube.a9070dd5.png"
+  },
+  {
+    "revision": "42ddf27e381fb408ca1c1f8275c68c29",
+    "url": "/portfolio/static/media/sut.42ddf27e.png"
+  },
+  {
+    "revision": "628b611de41ec380d7d292349824c328",
+    "url": "/portfolio/static/media/waterfall0.628b611d.png"
+  },
+  {
+    "revision": "dc02405d143d3f24d09c39f05ac9f197",
+    "url": "/portfolio/static/media/waterfall1.dc02405d.png"
+  },
+  {
+    "revision": "f4531fc74bd0754e4b33cf7f6a8bed45",
+    "url": "/portfolio/static/media/waterfall2.f4531fc7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cdf9acb6c2acbc67da7f551dc3775fa9.js"
+  "/portfolio/precache-manifest.bef0a729f83e1880045686d156ff0c24.js"
 );
 
 self.addEventListener('message', (event) => {
